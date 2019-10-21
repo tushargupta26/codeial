06054a42c3c8bd17f4a4b132fce396e29639c04e
tushargupta26 <43910109+tushargupta26@users.noreply.github.com>
2019-10-21 19:58:25 +0530

    setting up momgoose and mongodb

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const port=8000;
 const app=express();
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 app.use(express.static('./assets'));
 app.use(expressLayouts);
  // extract style and scripts from sub pages into the layout
