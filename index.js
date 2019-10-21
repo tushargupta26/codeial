a3c5a8f22a170194c883f9750e92d0f9b98dbc38
tushargupta26 <43910109+tushargupta26@users.noreply.github.com>
2019-10-21 19:37:44 +0530

    added static access for files(css,js,images)

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require ('express');
 const port=8000;
 const app=express();
 const expressLayouts = require('express-ejs-layouts');
-
+app.use(express.static('./assets'));
 app.use(expressLayouts);
+ // extract style and scripts from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 app.use(express.urlencoded());
 
 // use express router
