466fe7270e4f233fe0df2d00185490358dd9c314
tushargupta26 <43910109+tushargupta26@users.noreply.github.com>
2019-11-01 17:29:40 +0530

    add a feature to upload profile pic of user

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,8 +1,8 @@
 const User = require('../models/user');
 
 // let's keep it same as before
-module.exports.profile = function(req, res){
-    User.findById(req.params.id, function(err, user){
+module.exports.profile = function (req, res) {
+    User.findById(req.params.id, function (err, user) {
         return res.render('user_profile', {
             title: 'User Profile',
             profile_user: user
@@ -12,22 +12,43 @@ module.exports.profile = function(req, res){
 }
 
 
-module.exports.update = function(req, res){
-    if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+module.exports.update = async function (req, res) {
+    // if (req.user.id == req.params.id) {
+    //     User.findByIdAndUpdate(req.params.id, req.body, function (err, user) {
+    //         req.flash('success', 'Updated!');
+    //         return res.redirect('back');
+    //     });
+    // } else {
+    //     req.flash('error', 'Unauthorized!');
+    //     return res.status(401).send('Unauthorized');
+    // }
+    try{
+        let user = await User.findById(req.params.id);
+        User.uploadedAvatar(req,res,function(err){
+            if(err){
+                console.log('******* Error in Multer ***',err);
+                return;
+            }
+            user.name = req.body.name;
+            user.email = req.body.email;
+            if(req.file){
+                // this is saving the file uploaded into avatar
+                user.avatar = User.avatarPath + '/' + req.file.filename;
+            }
+            user.save();
             req.flash('success', 'Updated!');
             return res.redirect('back');
-        });
-    }else{
-        req.flash('error', 'Unauthorized!');
-        return res.status(401).send('Unauthorized');
+        })
+    }catch(err){
+        req.flash('error', err);
+        return res.redirect('back');
     }
 }
 
 
 // render the sign up page
-module.exports.signUp = function(req, res){
-    if (req.isAuthenticated()){
+module.exports.signUp = function (req, res) {
+    if (req.isAuthenticated()) {
         return res.redirect('/users/profile');
     }
 
@@ -39,9 +60,9 @@ module.exports.signUp = function(req, res){
 
 
 // render the sign in page
-module.exports.signIn = function(req, res){
+module.exports.signIn = function (req, res) {
 
-    if (req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         return res.redirect('/users/profile');
     }
     return res.render('user_sign_in', {
@@ -50,22 +71,30 @@ module.exports.signIn = function(req, res){
 }
 
 // get the sign up data
-module.exports.create = function(req, res){
-    if (req.body.password != req.body.confirm_password){
+module.exports.create = function (req, res) {
+    if (req.body.password != req.body.confirm_password) {
         req.flash('error', 'Passwords do not match');
         return res.redirect('back');
     }
 
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){req.flash('error', err); return}
+    User.findOne({
+        email: req.body.email
+    }, function (err, user) {
+        if (err) {
+            req.flash('error', err);
+            return
+        }
 
-        if (!user){
-            User.create(req.body, function(err, user){
-                if(err){req.flash('error', err); return}
+        if (!user) {
+            User.create(req.body, function (err, user) {
+                if (err) {
+                    req.flash('error', err);
+                    return
+                }
 
                 return res.redirect('/users/sign-in');
             })
-        }else{
+        } else {
             req.flash('success', 'You have signed up, login to continue!');
             return res.redirect('back');
         }
@@ -75,12 +104,12 @@ module.exports.create = function(req, res){
 
 
 // sign in and create a session for the user
-module.exports.createSession = function(req, res){
+module.exports.createSession = function (req, res) {
     req.flash('success', 'Logged in Successfully');
     return res.redirect('/');
 }
 
-module.exports.destroySession = function(req, res){
+module.exports.destroySession = function (req, res) {
     req.logout();
     req.flash('success', 'You have logged out!');
 
