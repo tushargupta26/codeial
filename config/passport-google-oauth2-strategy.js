2ae4b4c96197914f1c35929edb7a5e66d1431312
tushargupta26 <43910109+tushargupta26@users.noreply.github.com>
2019-11-03 00:02:21 +0530

    add google authentication

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,44 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const crypto = require('crypto');
+const User = require('../models/user');
+
+// tell passport to use a new strategy for google login
+passport.use(new googleStrategy({
+        clientID: "134510857434-iv18laj7l970nunoi0ikmq8u3d3h35tg.apps.googleusercontent.com",
+        clientSecret: "HrVbEVmP0M8CX7RsITS1lCzj",
+        callbackURL: "http://localhost:8000/users/auth/google/callback",
+    },
+
+    function (accessToken, refreshToken, profile, done) {
+        User.findOne({
+            email: profile.emails[0].value
+        }).exec(function (err, user) {
+            if (err) {
+                console.log("error in google strategy passport", err);
+                return;
+            };
+            console.log(accessToken , refreshToken);
+            console.log(profile);
+            if (user) {
+                // if found set this user as req.user
+                return done(null, user);
+            } else {
+                // if not found create the user and set it as req.user
+                User.create({
+                    name: profile.displayName,
+                    email: profile.emails,
+                    password: crypto.randomBytes(20).toString('hex')
+                }, function (err, user) {
+                    if (err) {
+                        console.log("error in creating user", err);
+                        return;
+                    };
+                    return done(null,user);
+                })
+            }
+        })
+    }
+))
+
+module.exports = passport;
